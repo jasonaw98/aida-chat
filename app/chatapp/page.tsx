062cad1341f0bb540159c61b4d9c9f9318ec730c
Jason Aw <jason_aw1998@hotmail.com>
2025-01-11 21:38:56 +0800

    add loading

--- app/chatapp/page.tsx
+++ app/chatapp/page.tsx
@@ -3,8 +3,14 @@ import Chats from "@/components/pages/Chats";
 import { Contacts } from "@/components/pages/Contacts";
 import { Profile } from "@/components/pages/Profile";
 import { TabsContent } from "@/components/ui/tabs";
+import { getUser } from "../(auth)/login/actions";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+  const { user } = await getUser();
+  if (!user) {
+    redirect("/");
+  }
   return (
     <div className="flex flex-col h-full">
       <div className="flex flex-col w-full h-full">
